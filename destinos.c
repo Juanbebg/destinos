8a0de5019030a73033d48c7f8f09a6371f88887c
Juanbebg <juanbe.bg@gmail.com>
2020-12-19 17:45:04 +0100

    Add files via upload

--- destinos.c
+++ destinos.c
@@ -7,6 +7,20 @@
 
 #include <stdio.h>
 
+#include <stdlib.h>
+int main(void) {
+FILE *fp = fopen("lorem.txt", "r");
+if(fp == NULL) {
+perror("Unable to open file!");
+exit(1);
+}
+char chunk[128];
+while(fgets(chunk, sizeof(chunk), fp) != NULL) {
+fputs("|*\n", stdout);  // marker string used to show where the content of the chunk array has ended
+}
+
+fclose(fp);
+}
 
 
 
